57be55e243a58e1882a64079286c3968fb74bf39
azimny86 <azimny86@gmail.com>
2021-07-06 12:25:31 +0000

    Connect Mongo DB

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask
+from flask_pymango import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MANGO_DBNAME"] = os.environ.get("MANGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
