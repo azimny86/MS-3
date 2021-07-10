598794117c479a7acd1d4920c7a981c516198ad6
azimny86 <azimny86@gmail.com>
2021-07-10 19:49:33 +0000

    Add login template and functionality

--- app.py
+++ app.py
@@ -44,6 +44,31 @@ def register():
         flash("Registration Successful")
     return render_template("register.html")
 
+
+@app.route("/login" , methods=["GET" , "POST"])
+def login():
+    if request.method == "POST":
+#existing user in dadabase
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+# checks if password matches user 
+
+            if check_password_hash(existing_user["password"] , request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome back , {}".format(request.form.get("username")))
+            else:
+# wrong password
+                flash("Wrong Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+# username don't exist in database
+            flash("Wrong Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 if __name__== "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
